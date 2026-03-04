c723bf20ab3274c93979439f97cb7ad45a4c71b7
Toy085 <brightabaka1@gmail.com>
2026-03-04 17:26:26 +0100

    Added basic variables

--- game.lua
+++ game.lua
@@ -2,6 +2,25 @@ local game = {}
 function game.load()
     centerX = love.graphics.getWidth() / 2
     centerY = love.graphics.getHeight() / 2
+
+    radius = 200
+    angle = 0
+    speed = 1.5
+    
+    playerDirection = 1
+
+    clickSound = love.audio.newSource("click.wav", "static")
+    music = love.audio.newSource("music.mp3", "stream")
+    music:play()
+
+    beatmap = {
+        {time = 5.0},
+        {time = 7.5},
+    }
+
+    score = 0
+    combo = 0
+    hitWindow = 0.3
 end
 
 function game.update(dt)
