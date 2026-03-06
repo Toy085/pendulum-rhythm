f16831befa0afde0370331f88322e967030a8597
Toy085 <brightabaka1@gmail.com>
2026-03-06 18:35:06 +0100

    Add saveBeatmap function to handle beatmap data saving

--- beatmap.lua
+++ beatmap.lua
@@ -4,6 +4,10 @@ function beatmap.loadBeatmap(filename)
     return beatmap
 end
 
+function beatmap.saveBeatmap(filename, beatmapData)
+    return beatmap
+end
+
 function beatmap.createBeatmap(filename)
     return beatmap
 end
